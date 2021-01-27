e2b4ec50119f1d6b941c631bd78916e0354a832c
arielsilvestre <77841278+arielsilvestre@users.noreply.github.com>
2021-01-27 20:48:46 -0300

    primer subida

---
+++ Proyecto1/js/scripts.js
@@ -0,0 +1,95 @@
+/*!
+    * Start Bootstrap - Freelancer v6.0.5 (https://startbootstrap.com/theme/freelancer)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+    */
+    (function($) {
+    "use strict"; // Start of use strict
+  
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: (target.offset().top - 71)
+          }, 1000, "easeInOutExpo");
+          return false;
+        }
+      }
+    });
+  
+    // Scroll to top button appear
+    $(document).scroll(function() {
+      var scrollDistance = $(this).scrollTop();
+      if (scrollDistance > 100) {
+        $('.scroll-to-top').fadeIn();
+      } else {
+        $('.scroll-to-top').fadeOut();
+      }
+    });
+  
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+  
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+      target: '#mainNav',
+      offset: 80
+    });
+  
+    // Collapse Navbar
+    var navbarCollapse = function() {
+      if ($("#mainNav").offset().top > 100) {
+        $("#mainNav").addClass("navbar-shrink");
+      } else {
+        $("#mainNav").removeClass("navbar-shrink");
+      }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+  
+    // Floating label headings for the contact form
+    $(function() {
+      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+      }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+      }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+      });
+    });
+  
+  })(jQuery); // End of use strict
+  
+  //var Usuario = new Object();
+  var listaUsuarios=[];
+  
+  function Usuario(nombre,email,telefono,mensaje){
+    this.nombre = nombre;
+    this.email= email;
+    this.telefono = telefono;
+    this.mensaje = mensaje;
+  }
+  var aux = new Usuario();
+  
+function contacto()
+{
+  var nombre = document.getElementById("name").value;
+  var mail = document.getElementById("email").value;
+  var tel = document.getElementById("phone").value;
+  var msg = document.getElementById("message").value;
+
+  aux  = new Usuario(nombre,mail,tel,msg);
+
+  usuarios.push(u);
+  console.log(usuarios);
+
+
+}
+
